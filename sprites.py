c093743b09df7286df3bde8bd6980973356e5e1a
andreasroald <andreas.roald@nettbrev.com>
2016-04-02 09:42:19 +0200

    Improved movement, changed some variable names
    If you press LEFT, and then RIGHT, the player will now not change direction before LEFT is released. max_speed is now top_speed, speed is now acceleration, and acceleration is now velocity

--- sprites.py
+++ sprites.py
@@ -13,60 +13,67 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (display_width/2, display_height/2)
 
-        self.moving_left = False
-        self.moving_right = False
-
-        self.max_speed = 8
+        self.moving = False
+        self.left_lock = False
+        self.right_lock = False
 
         self.acceleration = 0
+        self.top_speed = 8
+        self.velocity = 0
 
     # Player class event handling
     def events(self):
-        #Reset moving
+        #Reset moving & acceleration
         self.moving = False
-        speed = 0
-        
+        self.acceleration = 0
+
         # Movement keys handling
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        if keys[pygame.K_a] and not self.left_lock:
+            self.right_lock = True
             self.moving = True
-            speed = -0.5
-            self.accelerate(speed)
+            self.acceleration = -0.5
+            self.accelerate(self.acceleration)
+        else:
+            self.right_lock = False
 
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        if keys[pygame.K_d] and not self.right_lock:
+            self.left_lock = True
             self.moving = True
-            speed = 0.5
-            self.accelerate(speed)
+            self.acceleration = 0.5
+            self.accelerate(self.acceleration)
+        else:
+            self.left_lock = False
 
         if not keys[pygame.K_d] and not keys[pygame.K_a]:
-            if self.acceleration != 0:
+            if self.velocity != 0:
                 self.moving = True
-            self.accelerate(speed)
+            self.accelerate(self.acceleration)
 
     #Accelerate the player movement with acc_movement
     def accelerate(self, acc_movement):
         if acc_movement > 0:
-            if self.acceleration == self.max_speed:
-                self.acceleration = self.max_speed
-                
-            elif acc_movement < self.max_speed:
-                self.acceleration += acc_movement
+            if self.velocity == self.top_speed:
+                self.velocity = self.top_speed
+
+            elif acc_movement < self.top_speed:
+                self.velocity += acc_movement
 
         elif acc_movement < 0:
-            if self.acceleration == -self.max_speed:
-                self.acceleration = -self.max_speed
-                
-            elif acc_movement > -self.max_speed:
-                self.acceleration += acc_movement
+            if self.velocity == -self.top_speed:
+                self.velocity = -self.top_speed
 
-        #If acceleration is 0, slowly make acceleration slower        
+            elif acc_movement > -self.top_speed:
+                self.velocity += acc_movement
+
+        #If velocity is not 0, slowly make velocity slower
         else:
-            if self.acceleration != 0:
-                if self.acceleration > 0:
-                    self.acceleration -= 0.5
-                elif self.acceleration < 0:
-                    self.acceleration += 0.5  
+            if self.velocity != 0:
+                if self.velocity > 0:
+                    self.velocity -= 0.5
+                elif self.velocity < 0:
+                    self.velocity += 0.5
 
     # Update the player class
     def update(self):
@@ -74,7 +81,7 @@ class Player(pygame.sprite.Sprite):
 
         # Make player move
         if self.moving:
-            self.rect.x += self.acceleration
+            self.rect.x += self.velocity
 
     # Player drawing function
     def draw(self, display):
